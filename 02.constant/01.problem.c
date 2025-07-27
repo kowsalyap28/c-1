4ad7797084fedd618538b64c7a79f96065c72e23
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-07-27 18:00:42 +0530

    created CONSTANT

---
+++ 02.constant/01.problem.c
@@ -0,0 +1,10 @@
+// constant value cannot be changed 
+// for example 
+# include <stdio.h>
+void main()
+{
+int a =5;
+
+printf("%d",a);
+
+}
---
+++ 02.constant/02.problem.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+void main ()
+{
+float  g =9.8;
+printf("%f",g);
+
+}
