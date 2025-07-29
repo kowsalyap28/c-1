72224da85ad74917b7662f663bb4d81097e8de17
emailkowsalyax28@gmail.com <kowsalyax28@gmail.com>
2025-07-29 18:46:48 +0530

    created CONSTANT

---
+++ 03.if_statement/03.problem.c
@@ -0,0 +1,28 @@
+// NEASTED IF 
+# include <stdio.h>
+void main()
+{
+int a;
+printf("entre a:");
+scanf("%d",&a);
+if(a>=0)
+{
+if(a<5)
+{
+printf("a is greater than zero and less than five");
+
+}
+
+else
+{
+printf("a is greater than 5");
+
+}
+}
+else 
+{
+printf("a<0");
+
+}
+
+}
